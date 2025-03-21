33f1d04f4d7ea7b592145959b2f0ab53ad86a9c3
Vojtěch Toman <cry.wolf@centrum.cz>
2025-03-21 12:34:05 +0100

    gRPC Node: gracefull shutdown

--- node/src/grpc_node.rs
+++ node/src/grpc_node.rs
@@ -1,6 +1,9 @@
 use std::path::Path;
 
 use anyhow::{Context, Result};
+use tokio::signal::unix::{signal, SignalKind};
+use tokio_util::sync::CancellationToken;
+use tokio_util::task::TaskTracker;
 use tonic::transport::Server;
 
 use blockchain::BLOCKCHAIN;
@@ -76,6 +79,7 @@ async fn main() -> Result<()> {
         .parse()
         .with_context(|| format!("parsing address {listener_addr}"))?;
 
+    // gRPC Reflection Service
     let reflection = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(grpc::FILE_DESCRIPTOR_SET)
         .build_v1()
@@ -84,12 +88,64 @@ async fn main() -> Result<()> {
     println!("---");
     println!("Listening on {} (gRPC)", addr);
 
-    Server::builder()
+    // Graceful shutdown
+    let token = CancellationToken::new();
+    let cancel_token = token.clone();
+    let tracker = TaskTracker::new();
+
+    // start a task to periodically cleanup the mempool
+    tracker.spawn(blockchain::cleanup(cancel_token.clone()));
+    // and a task to periodically save the blockchain
+    tracker.spawn(blockchain::save(
+        blockchain_file.clone(),
+        cancel_token.clone(),
+    ));
+
+    let tracker_clone = tracker.clone();
+    tokio::spawn(async move {
+        let mut hup = signal(SignalKind::hangup()).unwrap();
+        let mut term = signal(SignalKind::terminate()).unwrap();
+        tokio::select! {
+            _ = tokio::signal::ctrl_c() => {
+                println!("\n>>> ctrl-c received!");
+                tracker_clone.close();
+                token.cancel();
+            }
+           _= hup.recv() => {
+                println!(">>> got signal HUP");
+                tracker_clone.close();
+                token.cancel();
+            }
+            _= term.recv() => {
+                println!(">>> got signal TERM");
+                tracker_clone.close();
+                token.cancel();
+            }
+        }
+    });
+
+    tokio::select! {
+        _ = cancel_token.cancelled() => {
+            println!("graceful shutdown triggered");
+            println!("terminating server");
+        }
+        // Build and start gRPC server
+        res = Server::builder()
         .add_service(reflection)
         .add_service(grpc::node_api::create_server(peers))
         .add_service(grpc::miner_api::create_server())
         .serve(addr)
-        .await?;
+         => {
+            res?;
+        }
+    }
+
+    println!("server ended, waiting for tasks to complete...");
+
+    tracker.wait().await;
+
+    println!("all tasks exited");
+    println!("node stopped");
 
     Ok(())
 }
